dfde43b8676eb6d15ba91da9e8dd30b06aa80969
joscul <josefcullhed@gmail.com>
2022-06-28 23:28:58 +0200

    Added dark mode logo.

--- menu.php
+++ menu.php
@@ -3,6 +3,9 @@
 		<a href="/settings">Settings</a>
 		<a href="/about">About</a>
 	</nav>
-	<h1><a href="/"><img src="/assets/alexandria.svg" alt="Alexandria.org" /></a></h1>
+	<h1><a href="/">
+		<img src="/assets/alexandria.svg" alt="Alexandria.org" class="logo-light" />
+		<img src="/assets/alexandria-dark.svg" alt="Alexandria.org" class="logo-dark" />
+	</a></h1>
 	<small>Search the web with alexandria.org</small>
 </div>
