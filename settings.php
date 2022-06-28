05a6bfb7e4532228b0c459d4324b71369682182e
joscul <josefcullhed@gmail.com>
2022-06-28 14:21:28 +0200

    Added dark mode.

--- settings.php
+++ settings.php
@@ -13,6 +13,7 @@ include("menu.php");
 	<p>We value privacy, so you can disable all tracking and use our service completely anonymously by unticking the checkboxes below.</p>
 
 	<form>
+		<b>Privacy settings</b>
 		<div>
 			<label>Store IP address with search queries: </label>
 			<input type="checkbox" id="store_ip" true-value="1" false-value="0">
@@ -21,6 +22,14 @@ include("menu.php");
 			<label>Store clicks on search results together with search query: </label>
 			<input type="checkbox" id="store_clicks" true-value="1" false-value="0">
 		</div>
+
+		<b>UI Settings</b>
+		<div>
+			<label>Enable dark mode: </label>
+			<input type="checkbox" id="theme_dark" true-value="1" false-value="0">
+		</div>
+
+		<br/>
 
 		<div>
 			<button id="save" disabled>Save</button>
